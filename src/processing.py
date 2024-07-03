203228188aa140afc2d86d1e7db30b8a03a81629
Ivan <rastorgu98@gmail.com>
2024-07-03 17:54:27 +0300

    add fainl

--- src/processing.py
+++ src/processing.py
@@ -17,4 +17,15 @@ def filter_by_state(inform_state: list[dict[str]], state='CANCELED') -> Any:
     return list_state
 
 
+def sort_by_date(list_of_dict: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """
+    Функция принимает на вход список словарей и возвращает новый список, в котором исходные
+    словари отсортированы по убыванию даты
+    """
+    inform_state = sorted(
+        list_of_dict, kay=lambda new_list_of_dict: new_list_of_dict["date"], reverse=reverse
+    )
+    return inform_state
+
+
 print(filter_by_state(inform_state))
