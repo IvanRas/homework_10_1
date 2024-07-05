e9f771ee85c17ea30edfabb8a4021acfcd61e7ef
Ivan <rastorgu98@gmail.com>
2024-07-05 09:22:21 +0300

    error correction 10_1

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты."""
-    return f'{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}'
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-def get_mask_account(macc_number: str) -> str | None:
+def get_mask_account(macc_number: str) -> str:
     """Функция маскировки номера счета."""
     mask_account = macc_number[:5] + "**" + macc_number[-4:]
     return mask_account
--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
-from typing import Any, List, Dict
+from typing import Any
 
 operations = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 
-def filter_by_state(operations: list[dict[str]], state='EXECUTED') -> Any:
+def filter_by_state(operations: list[dict[str]], state="EXECUTED") -> Any:
     """
     Функция принимает список словарей и опционально значение для ключа
     state и возвращает новый список словарей, содержащий только те словари, у которых ключ
@@ -21,12 +21,12 @@ def filter_by_state(operations: list[dict[str]], state='EXECUTED') -> Any:
     return filtered_operations
 
 
-def sort_by_date(operations: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
     """
     Функция принимает на вход список словарей и возвращает новый список, в котором исходные
     словари отсортированы по убыванию даты
     """
-    operations = sorted( operations, kay = lambda new_list_of_dict: new_list_of_dict["date"], reverse = True)
+    operations = sorted(operations, key=lambda new_list_of_dict: new_list_of_dict["date"], reverse=reverse)
     return operations
 
 
