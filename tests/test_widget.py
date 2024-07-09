71c06a7c4242fa5e6a3d40e7cf927cf988cb4194
Ivan <rastorgu98@gmail.com>
2024-07-09 09:59:42 +0300

    added a test

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -8,3 +8,14 @@ from src.widget import mask_account_card, get_date
 ])
 def test_mask_account_card(string, expected_result):
     assert mask_account_card(string) == expected_result
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ('2018-07-11T02:26:18.671407', '11.07.2018'),
+        ('2018-07-12T13:26:18.671407', '12.07.2018'),
+    ]
+)
+def test_get_data(value, expected):
+    assert get_date(value) == expected
