9fa198dbd74722b65a75438b724ed15404919ad1
Ivan Rastorguev <rastorgu98@gmail.com>
2024-07-23 14:19:08 +0300

    Revert "Homework 12 1"

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,12 @@
 import json
 
 
-def get_transactions_dictionary(path: str) -> dict:
+def get_transactions_dictionary():
     """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
-        # with open(path, "r", "operations.json", encoding='utf-8') as operations:
-        with open(path, "r", encoding='utf-8') as operations:
+        with open("operations.json") as operations:
             try:
-                transactions_data = json.load(operations)
+                transactions_data = json.loads(operations)
                 return transactions_data
             except json.JSONDecodeError:
                 transactions_data = []
@@ -15,4 +14,3 @@ def get_transactions_dictionary(path: str) -> dict:
     except FileNotFoundError:
         transactions_data = []
         return transactions_data
-        # говорит о возрощений пипа list
