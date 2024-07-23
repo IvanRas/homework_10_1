5531dd4497d7bdc009fd3347fee7dc6493550a92
Ivan <rastorgu98@gmail.com>
2024-07-23 09:06:24 +0300

    ДЗ 12_1 путь json

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,15 @@
 import json
+from typing import Any
+
+def get_transactions_dictionary():
+    """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    try:
+        with open('operations.json') as operations:
+            transactions_data = json.loads(operations)
+            return transactions_data
+        except TypeError:
+            transactions_data =[]
+            return transactions_data
+    except ValueError:
+        transactions_data = []
+        return transactions_data
