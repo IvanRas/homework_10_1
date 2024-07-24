630cb26969f6824da1b85a1f701327b66e47914e
Ivan <rastorgu98@gmail.com>
2024-07-24 13:35:41 +0300

    ДЗ 12_1 редакция utils, external_api, добавление тестов

--- src/external_api.py
+++ src/external_api.py
@@ -1,5 +1,4 @@
 import os
-from typing import Any
 
 
 import requests
@@ -10,16 +9,32 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 
-def convert_from_usd_to_rub(amount: float) -> Any:
+def convert_from_i_to_rub(amount: float, currency: str) -> float or str:
     """Функция принимает значение в долларах, обращается к API и возвращает конвертацию в рубли/"""
     # amount == operations["operationAmount"]["currency"]["code": "RUB"]
-    if amount == ["operationAmount"]["currency"]["code": "RUB"]:
+    # url = "https://api.apilayer.com/exchangerates_data/convert?to=to&from=from&amount=amount"
+    if currency == ["operationAmount"]["currency"]["code": "RUB"]:
         return amount
-    elif amount == ["operationAmount"]["currency"]["code": "USD"]:
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to=rub&from=USD&amount={amount}"
+    elif currency == ["operationAmount"]["currency"]["code": "USD"]:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
         headers = {"apikey": api_key}
         response = requests.request("GET", url, headers=headers)
         result = response.text
-        return result
+        json_result = response.json()
+        print(json_result)
+        rub_amount = json_result["result"]
+        print(json_result["result"])
+        return rub_amount
+    elif currency == ["operationAmount"]["currency"]["code": "EUR"]:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        headers = {"apikey": api_key}
+        response = requests.request("GET", url, headers=headers)
+        result = response.text
+        json_result = response.json()
+        print(json_result)
+        rub_amount = json_result["result"]
+        print(json_result["result"])
+        return rub_amount
     else:
         return "Неизвесная волюта"
+        # # говорит о возрощений пипа str, ошибка из за того то не float
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import json
 
 
-def get_transactions_dictionary(path: str) -> dict:
+def get_transactions_dictionary(path: str) -> dict or list:
+# def get_transactions_dictionary(path: str) -> list:
     """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
-        with open(path, "r", "operations.json") as operations:
+        # with open(path, "r", "operations.json", encoding='utf-8') as operations:
+        with open(path, "r", encoding='utf-8') as operations:
             try:
-                transactions_data = json.loads(operations)
+                transactions_data = json.load(operations)
                 return transactions_data
             except json.JSONDecodeError:
                 transactions_data = []
@@ -14,3 +16,4 @@ def get_transactions_dictionary(path: str) -> dict:
     except FileNotFoundError:
         transactions_data = []
         return transactions_data
+        # говорит о возрощений пипа list
