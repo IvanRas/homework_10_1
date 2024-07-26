8dedf566b95e9be7f65a361820a7e691cf836c52
Ivan <rastorgu98@gmail.com>
2024-07-26 16:33:16 +0300

    ДЗ 12_1 редакция tests/

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,11 +1,64 @@
 import pytest
+
 from src.external_api import convert_from_i_to_rub
 
 @pytest.fixture
 def get_rub_transaction():
-    return "RUB", 123.45
+    return 123.45
+
+@pytest.fixture
+def get_usd_transaction():
+    return 123.45
 
 
-def test_convert_from_i_to_rub(get_rub_transaction):
-    assert convert_from_i_to_rub("RUB", 123.45) == 123.45
-    
+transaction = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+        "amount": 123.45,
+        "currency": {
+          "name": "руб.",
+          "code": "RUB"
+        }
+    }
+}
+
+transaction_usr = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+        "amount": 2.0,
+        "currency": {
+          "name": "USD.",
+          "code": "USD"
+        }
+    }
+}
+
+
+transaction_r = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+        "amount": 2.0,
+        "currency": {
+          "name": "USD.",
+          "code": "r"
+        }
+    }
+}
+
+
+def test_convert_from_rub_to_rub(get_rub_transaction):
+    assert convert_from_i_to_rub(transaction) == 123.45
+
+
+def test_convert_from_not_to_rub(get_usd_transaction):
+    assert convert_from_i_to_rub(transaction_r) == "Неизвесная волюта"
+
+
+def test_convert_from_not_rub_to_rub(get_usd_transaction):
+    assert convert_from_i_to_rub(transaction_usr) == 174.62
