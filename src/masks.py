ae694064ddd2fe6d39b1564d37363dd2c6c9d122
root <rastorgu98@gmail.com>
2024-07-30 12:21:24 +0300

    DZ_12_2 creature log 2v

--- src/masks.py
+++ src/masks.py
@@ -3,19 +3,29 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s",
-    filename="../logs/masks.log",
+    filename='../logs/masks.log',
+    # filename='masks.log',
     filemode="w",
 )
+auth_logger = logging.getLogger('app.auth')
+# logging.basicConfig(
+#     logger = logging.getLogger(__name__)
+#     file_handler = logging.FileHandler('masks.log')
+#     file_formatter = "%(asctime)s: %(filename)s: %(levelname)s: %(message)s",
+#     file_handler.setFormatter(file_formatter)
+#     logger.addHandler(file_handler)
+#     logger.setLevel(logging.INFO)
+# )
 
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты."""
-    logging.info(f"Маска карты{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}")
+    auth_logger.info(f"Маска карты{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(macc_number: str) -> str:
     """Функция маскировки номера счета."""
     mask_account = "**" + macc_number[-4:]
-    logging.info(f"Маска счета{mask_account}")
+    auth_logger.info(f"Маска счета{mask_account}")
     return mask_account
--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,15 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s",
     filename="../logs/utils.log",
+    # filename="utils.log",
     filemode="w",
 )
+auth_logger = logging.getLogger('app.auth')
+# logger = logging.getLogger(__name__)
+# file_handler = logging.FileHandler('utils.log')
+# logger.addHandler(file_handler)
+# logger.setLevel(logging.INFO)
+
 
 def get_transactions_dictionary(path: str) -> dict:
     """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
@@ -15,15 +22,15 @@ def get_transactions_dictionary(path: str) -> dict:
         # with open(path, "r", "operations.json", encoding='utf-8') as operations:
         with open(path, "r", encoding='utf-8') as operations:
             try:
-                logging.info("Информация по счету")
+                auth_logger.info("Информация по счету")
                 transactions_data = json.load(operations)
                 return transactions_data
             except json.JSONDecodeError:
                 transactions_data = []
-                logging.info("Информация по счету не удачно")
+                auth_logger.info("Информация по счету не удачно")
                 return transactions_data
     except FileNotFoundError:
         transactions_data = []
-        logging.info("Файл поврежден")
+        auth_logger.info("Файл поврежден")
         return transactions_data
         # говорит о возрощений пипа list
